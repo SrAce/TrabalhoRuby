562376393e3c9c4ddcc643011d446cca9007769d
vitoraraujo <vitoraraujow@gmail.com>
2017-05-12 15:42:57 -0300

    total da conta aprecendo - item 10

--- waterworks/app/helpers/waterbills_helper.rb
+++ waterworks/app/helpers/waterbills_helper.rb
@@ -1,2 +1,17 @@
 module WaterbillsHelper
+	def teste ( a )
+		if a >= 11 && a <= 30
+			(a - 30) + 7
+			if a < 0
+				(a - 11) + 7
+			end
+		elsif a >= 31 && a <= 100
+			((a - 100)*2) + (a - 30) + 7
+			if a < 0
+				((a - 31)*2) + (a - 30) + 7
+			end
+		elsif a >= 101
+			(a - 30) + ((a - 100)*2) + ((a - 101)*5) + 7
+		end			
+	end
 end
